485820408b8f24bc3a5f4ea26f3eff7fba57a279
Steve <steve.f.yeager@gmail.com>
2021-08-30 19:58:55 -0700

    Setup scaffolding

---
+++ Articlib/Articles/Test/Articlib.Articles.Domain.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Articlib.Articles.Domain.Test;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
