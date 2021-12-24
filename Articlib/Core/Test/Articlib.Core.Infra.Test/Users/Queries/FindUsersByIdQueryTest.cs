918bb7481495c65fe6e8123b18e408a2ee1a8b5e
Steve <steve.f.yeager@gmail.com>
2021-12-23 20:00:34 -0800

    Update tests to use new Common helpers

--- Articlib/Core/Test/Articlib.Core.Infra.Test/Users/Queries/FindUsersByIdQueryTest.cs
+++ Articlib/Core/Test/Articlib.Core.Infra.Test/Users/Queries/FindUsersByIdQueryTest.cs
@@ -1,89 +1,9 @@
-﻿using System.Linq.Expressions;
-using Articlib.Core.Infra.Users;
-using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+﻿using Articlib.Core.Infra.Users;
+using LittleByte.Extensions.AutoMapper;
+using LittleByte.Test.EntityFramework;
 using NUnit.Framework;
 
 namespace Articlib.Core.Infra.Test.Users.Queries;
-
-// TODO: Move to test lib.
-public class NullObjectMapper : IMapper
-{
-    public TDestination Map<TDestination>(object source)
-    {
-        return default!;
-    }
-
-    public TDestination Map<TSource, TDestination>(TSource source)
-    {
-        return default!;
-    }
-
-    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
-    {
-        return default!;
-    }
-
-    public object Map(object source, Type sourceType, Type destinationType)
-    {
-        return default!;
-    }
-
-    public object Map(object source, object destination, Type sourceType, Type destinationType)
-    {
-        return default!;
-    }
-
-    public TDestination Map<TDestination>(object source, Action<IMappingOperationOptions<object, TDestination>> opts)
-    {
-        return default!;
-    }
-
-    public TDestination Map<TSource, TDestination>(TSource source,
-        Action<IMappingOperationOptions<TSource, TDestination>> opts)
-    {
-        return default!;
-    }
-
-    public TDestination Map<TSource, TDestination>(TSource source, TDestination destination,
-        Action<IMappingOperationOptions<TSource, TDestination>> opts)
-    {
-        return default!;
-    }
-
-    public object Map(object source, Type sourceType, Type destinationType,
-        Action<IMappingOperationOptions<object, object>> opts)
-    {
-        return default!;
-    }
-
-    public object Map(object source, object destination, Type sourceType, Type destinationType,
-        Action<IMappingOperationOptions<object, object>> opts)
-    {
-        return default!;
-    }
-
-    public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source, object? parameters = null,
-        params Expression<Func<TDestination, object>>[] membersToExpand)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<TDestination> ProjectTo<TDestination>(IQueryable source, IDictionary<string, object> parameters,
-        params string[] membersToExpand)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable ProjectTo(IQueryable source, Type destinationType, IDictionary<string, object>? parameters = null,
-        params string[] membersToExpand)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IConfigurationProvider ConfigurationProvider => null!;
-    public Func<Type, object> ServiceCtor => null!;
-}
 
 public class FindUsersByIdQueryTest
 {
@@ -93,10 +13,7 @@ public class FindUsersByIdQueryTest
     [SetUp]
     public void SetUp()
     {
-        var options = new DbContextOptionsBuilder<UsersContext>()
-            .UseInMemoryDatabase("Test")
-            .Options;
-        usersContext = new UsersContext(options);
+        DbContextFactory.InMemory(ref usersContext!);
         var mapper = new NullObjectMapper();
         testObj = new FindUsersByIdQuery(usersContext, mapper);
     }
@@ -105,9 +22,8 @@ public class FindUsersByIdQueryTest
     public async Task Given_AllUniqueIds_Then_IterateAll()
     {
         var entities = TV.Users.NewUsers(2);
-        usersContext.Users.AddRange(entities);
-        await usersContext.SaveChangesAsync();
-        
+        usersContext.AddRangeAndSave(entities);
+
         var ids = entities.Select(u => u.Id).ToArray();
 
         var result = await testObj.SendAsync(ids);
@@ -120,10 +36,9 @@ public class FindUsersByIdQueryTest
     public async Task Given_DupeIds_Then_Dedupe()
     {
         var entity = TV.Users.NewUser();
-        usersContext.Users.Add(entity);
-        await usersContext.SaveChangesAsync();
+        usersContext.AddAndSave(entity);
 
-        var ids = new []{entity.Id, entity.Id};
+        var ids = new[] {entity.Id, entity.Id};
 
         var result = await testObj.SendAsync(ids);
 
