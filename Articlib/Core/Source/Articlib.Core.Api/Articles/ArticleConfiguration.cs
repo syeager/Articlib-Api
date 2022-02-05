0addea6ddd76b69522863693eef37734506cdfef
Rachel Simm <rsimm.cs@gmail.com>
2022-02-05 14:27:08 -0800

    Fix DI for admin endpoints

--- Articlib/Core/Source/Articlib.Core.Api/Articles/ArticleConfiguration.cs
+++ Articlib/Core/Source/Articlib.Core.Api/Articles/ArticleConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Articlib.Core.Domain.Articles;
+using Articlib.Core.Api.Users;
+using Articlib.Core.Domain.Articles;
 using Articlib.Core.Domain.Votes.Services;
 using LittleByte.Validation;
 
@@ -9,6 +10,8 @@ internal static class ArticleConfiguration
     public static IServiceCollection AddArticles(this IServiceCollection @this)
     {
         return @this
+            .AddTransient<CreateArticleController>()
+            .AddTransient<LogInController>()
             .AddTransient<IAddVote, AddVote>()
             .AddTransient<IModelValidator<Article>, ArticleValidator>()
             .AddTransient<IArticleCreateService, ArticleCreateService>();
