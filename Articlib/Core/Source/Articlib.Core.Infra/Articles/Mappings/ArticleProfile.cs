68b722e197b669c402a77a7112306b5e640199be
Steve Yeager <steve.f.yeager@gmail.com>
2022-02-07 22:22:59 -0800

    Fix article profile after change to convert to Valid<Article>

--- Articlib/Core/Source/Articlib.Core.Infra/Articles/Mappings/ArticleProfile.cs
+++ Articlib/Core/Source/Articlib.Core.Infra/Articles/Mappings/ArticleProfile.cs
@@ -1,11 +1,14 @@
-﻿using Articlib.Core.Domain.Articles;
+using System.Diagnostics.CodeAnalysis;
+using Articlib.Core.Domain.Articles;
 using Articlib.Core.Domain.Votes.Models;
 using Articlib.Core.Infra.Articles.Models;
 using Articlib.Core.Infra.Votes.Models;
 using AutoMapper;
+using LittleByte.Validation;
 
 namespace Articlib.Core.Infra.Articles.Mappings;
 
+[SuppressMessage("ReSharper", "UnusedType.Global")]
 public sealed class ArticleProfile : Profile
 {
     public ArticleProfile()
@@ -14,7 +17,7 @@ public sealed class ArticleProfile : Profile
         CreateMap<string, Uri>().ConvertUsing(s => new Uri(s));
 
         CreateMap<Article, ArticleDao>();
-        CreateMap<ArticleDao, Article>().DisableCtorValidation().ConvertUsing<ArticleConverter>();
+        CreateMap<ArticleDao, Valid<Article>>().DisableCtorValidation().ConvertUsing<ArticleConverter>();
 
         CreateMap<VoteDao, Vote>();
         CreateMap<Vote, VoteDao>();
