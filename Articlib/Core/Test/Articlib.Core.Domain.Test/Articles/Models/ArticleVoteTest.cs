c4b5ce14bd066f1b09bdc2f940e8606144d42c28
Steve Yeager <steve.f.yeager@gmail.com>
2022-02-12 07:27:31 -0800

    Add article vote overflow tests

---
+++ Articlib/Core/Test/Articlib.Core.Domain.Test/Articles/Models/ArticleVoteTest.cs
@@ -0,0 +1,45 @@
+using Articlib.Core.Domain.Articles.Exceptions;
+using NUnit.Framework;
+
+namespace Articlib.Core.Domain.Test.Articles;
+
+public class ArticleVoteTest
+{
+    [Test]
+    public void When_VoteAdded_Then_VoteCountIncreasedBy1()
+    {
+        var article = TV.Articles.Valid().GetModelOrThrow();
+
+        var initialValue = article.VoteCount;
+        article.AddVote();
+
+        Assert.AreEqual(initialValue + 1, article.VoteCount);
+    }
+
+    [Test]
+    public void When_VoteCountExceedsMax_Then_Throw()
+    {
+        var article = TV.Articles.Valid(uint.MaxValue).GetModelOrThrow();
+
+        Assert.Throws<ArticleVoteOverflowException>(() => article.AddVote());
+    }
+
+    [Test]
+    public void When_VoteRemoved_Then_VoteCountDecreasedBy1()
+    {
+        var article = TV.Articles.Valid(1).GetModelOrThrow();
+
+        var initialValue = article.VoteCount;
+        article.RemoveVote();
+
+        Assert.AreEqual(initialValue - 1, article.VoteCount);
+    }
+
+    [Test]
+    public void When_VoteCountExceedsMin_Then_Throw()
+    {
+        var article = TV.Articles.Valid().GetModelOrThrow();
+
+        Assert.Throws<ArticleVoteOverflowException>(() => article.RemoveVote());
+    }
+}
